91f4b9812428a6ce04ac2cae48a8bfbd26c224b4
pablopierre00 <00001134338673SP@al.educacao.sp.gov.br>
2024-09-11 10:06:23 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {createCanvas(600, 600);
+  background("black")}
+
+
+function draw() {
+  stroke("blue");
+  fill("red");
+  
+  if(mouseIsPressed){
+    rect(mouseX,mouseY, 15, 20)
+}}
